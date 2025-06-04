a8c65f2eccda2df9e5ba3790e434786344a2bf62
Samar Khan <skhan511@umd.edu>
2025-06-04 12:00:14 -0500

    fantasy ui changes from figma

--- src/pages/Fantasy.tsx
+++ src/pages/Fantasy.tsx
@@ -1,29 +1,72 @@
-import RaceCard from "../components/race-picker/RaceCard";
+import RaceCard from "../components/race-card/RaceCard";
+import { useRaceStore } from "../store/RaceStore";
 import { useState } from "react";
 import { Race } from "../types/race";
 import { racelist } from "../data/racelist";
+import RaceHeader from "../components/race-header/RaceHeader";
+import CardStack from "../components/card-stack/CardStack";
+import DriverCard from "../components/driver-card/DriverCard";
 
 const Fantasy = () => {
   const [races] = useState<Race[]>(racelist);
 
-  const [selectedRaceId, setSelectedRaceId] = useState<number | null>(null);
+  const { selectedRace, isEditing, selectRace, toggleEdit } = useRaceStore();
 
-  const handleRaceSelect = (raceId: number) => {
-    setSelectedRaceId(raceId);
-  };
+  // const [selectedRace, setSelectedRace] = useState<Race>(racelist[0]);
+  // const [isEditing, setIsEditing] = useState(false);
+
+  // const handleRaceSelect = (race: Race) => {
+  //   setSelectedRace(race);
+  // };
+
+  // const toggleEdit = () => {
+  //   setIsEditing((prev) => !prev);
+  // };
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-4 p-1">
-        {races.map((race) => (
-          <RaceCard
-            key={race.id}
-            race={race}
-            isSelected={race.id === selectedRaceId}
-            onSelect={handleRaceSelect}
+      {!isEditing ? (
+        <>
+          <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-4 p-1">
+            {races.map((race) => (
+              <RaceCard
+                key={race.id}
+                race={race}
+                isSelected={race.id === selectedRace.id}
+                onSelect={selectRace}
+              />
+            ))}
+          </div>
+          <RaceHeader
+            raceName={`${selectedRace.country} GP` || "Select a race"}
+            raceDate={new Date(selectedRace.date)}
+            costCap={100.0}
+            onEdit={toggleEdit}
+            isEditing={isEditing}
           />
-        ))}
-      </div>
+          <div className="flex items-center justify-center">
+            <CardStack
+              cards={[
+                <DriverCard />,
+                <DriverCard />,
+                <DriverCard />,
+                <DriverCard />,
+              ]}
+            />
+            {/* <img src="src\assets\card.png" className="rounded-2xl h-80" /> */}
+          </div>
+        </>
+      ) : (
+        <>
+          <RaceHeader
+            raceName={`${selectedRace.country} GP` || "Select a race"}
+            raceDate={new Date(selectedRace.date)}
+            costCap={100.0}
+            onEdit={toggleEdit}
+            isEditing={isEditing}
+          />
+        </>
+      )}
     </div>
   );
 };
