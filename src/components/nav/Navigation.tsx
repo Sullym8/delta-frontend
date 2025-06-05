f63acaadbbd4e908548f058c03f68285274aed61
Samar Khan <skhan511@umd.edu>
2025-06-05 11:21:56 -0500

    padding

--- src/components/nav/Navigation.tsx
+++ src/components/nav/Navigation.tsx
@@ -12,7 +12,7 @@ const Navigation = () => {
   };
 
   return (
-    <div className="container mx-auto p-2">
+    <div className="container mx-auto p-2 mb-1">
       <p className="font-black text-3xl text-center mb-2">
         <Link
           to={"/"}
