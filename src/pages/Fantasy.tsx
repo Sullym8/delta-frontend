99075bde781c8397b4b8280af05c176882223aa7
Samar Khan <skhan511@umd.edu>
2025-06-05 11:22:39 -0500

    zustand migration

--- src/pages/Fantasy.tsx
+++ src/pages/Fantasy.tsx
@@ -6,34 +6,36 @@ import { racelist } from "../data/racelist";
 import RaceHeader from "../components/race-header/RaceHeader";
 import CardStack from "../components/card-stack/CardStack";
 import DriverCard from "../components/driver-card/DriverCard";
+import DriverCardMini from "../components/driver-card-mini/DriverCardMini";
+import DriverSelector from "../components/driver-selector/DriverSelector";
 
 const Fantasy = () => {
   const [races] = useState<Race[]>(racelist);
 
-  const { selectedRace, isEditing, selectRace, toggleEdit } = useRaceStore();
+  const {
+    selectedRace,
+    isEditing,
+    selectedDrivers,
+    selectRace,
+    toggleEdit,
+    removeDriver,
+  } = useRaceStore();
 
-  // const [selectedRace, setSelectedRace] = useState<Race>(racelist[0]);
-  // const [isEditing, setIsEditing] = useState(false);
-
-  // const handleRaceSelect = (race: Race) => {
-  //   setSelectedRace(race);
-  // };
-
-  // const toggleEdit = () => {
-  //   setIsEditing((prev) => !prev);
-  // };
+  const handleSelectRace = (race: Race) => {
+    selectRace(race);
+  };
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-2.5">
       {!isEditing ? (
         <>
-          <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-4 p-1">
+          <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-2.5">
             {races.map((race) => (
               <RaceCard
                 key={race.id}
                 race={race}
                 isSelected={race.id === selectedRace.id}
-                onSelect={selectRace}
+                onSelect={handleSelectRace}
               />
             ))}
           </div>
@@ -45,22 +47,12 @@ const Fantasy = () => {
           />
           <div className="flex items-center justify-center">
             <CardStack
-              cards={[
+              cards={selectedDrivers.map((driver) => (
                 <DriverCard
-                  driverName={""}
-                  teamName={""}
-                  rating={0}
-                  race={0}
-                  consistency={0}
-                  reliability={0}
-                  cost={0}
-                  deltaCost={0}
-                  AI_Qualifying={""}
-                  AI_Race={""}
-                  driverImage={""}
-                  teamImage={""}
-                />,
-              ]}
+                  key={driver.driverCode}
+                  driverName={driver.driverName}
+                />
+              ))}
             />
             {/* <img src="src\assets\card.png" className="rounded-2xl h-80" /> */}
           </div>
@@ -73,6 +65,16 @@ const Fantasy = () => {
             costCap={100.0}
             onEdit={toggleEdit}
           />
+          <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-2">
+            {selectedDrivers.map((driver) => (
+              <DriverCardMini
+                key={driver.driverCode}
+                driverCode={driver.driverCode}
+                deleteDriver={removeDriver}
+              />
+            ))}
+          </div>
+          <DriverSelector />
         </>
       )}
     </div>
