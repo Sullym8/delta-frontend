7601c768b69667bab7382a7d9bd70bed29f097ba
Samar Khan <skhan511@umd.edu>
2025-06-27 04:17:42 -0400

    updated racecard ui + added animations on scroll

--- src/components/race-card/RaceCard.tsx
+++ src/components/race-card/RaceCard.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Race } from "../../types/race";
 
 interface RaceCardProps {
@@ -8,30 +9,37 @@ interface RaceCardProps {
 
 const RaceCard = ({ race, isSelected, onSelect }: RaceCardProps) => {
   const baseClasses =
-    "box-border m-0.5 p-4 flex flex-col justify-between ring-1 w-44 rounded-2xl flex-shrink-0 ring-white/10 transition-all duration-200 ease-in-out";
+    "box-border m-0.5 p-4 flex flex-col justify-between ring-1 w-48 rounded-2xl flex-shrink-0 ring-white/10 transition-all duration-200 ease-in-out gap-1";
   const variableClasses = isSelected
     ? "bg-delta-active"
     : "bg-delta-container-bg";
   return (
-    <div
-      onClick={() => onSelect(race)}
-      className={`${baseClasses} ${variableClasses}`}
-    >
-      <div className="flex flex-row justify-between">
-        <img
-          src={`https://flagcdn.com/40x30/${race.countryCode}.png`}
-          width="20"
-          height="15"
-          alt="Bahrain"
-          className="self-center"
-        />
-        <p className="font-black font-[Unbounded]">{`R${race.round}`}</p>
+    <motion.div whileTap={{ scale: 0.95 }}>
+      <div
+        onClick={() => onSelect(race)}
+        className={`${baseClasses} ${variableClasses}`}
+      >
+        <div className="flex flex-row justify-between items-center">
+          <img
+            src={`https://flagcdn.com/40x30/${race.countryCode}.png`}
+            width="20"
+            height="15"
+            alt="Bahrain"
+            className="self-center"
+          />
+          <div className="flex flex-col items-center px-2 py-1 rounded-full bg-delta-accent">
+            <span className="text-xs font-bold">{`R${race.round}`}</span>
+          </div>
+          {/* <p className="font-black font-[Unbounded]">{`R${race.round}`}</p> */}
+        </div>
+        <div>
+          <p className="text-2xl font-black truncate font-[Unbounded]">
+            {race.country}
+          </p>
+          <p className="text-xs ">03 Mar - 05 Mar</p>
+        </div>
       </div>
-      <p className="text-2xl font-black truncate font-[Unbounded]">
-        {race.country}
-      </p>
-      <p className="text-xs ">03 Mar - 05 Mar</p>
-    </div>
+    </motion.div>
   );
 };
 
