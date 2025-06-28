aafb173671ba52845399c7ac3c6987cb45719f32
Samar Khan <skhan511@umd.edu>
2025-06-28 05:31:25 -0400

    added loading

--- src/pages/Fantasy.tsx
+++ src/pages/Fantasy.tsx
@@ -64,98 +64,102 @@ const Fantasy = () => {
   return (
     <>
       <div className="flex flex-col gap-2.5 h-full">
-        {!isEditing ? (
-          <>
-            <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-2.5 flex-shrink-0">
-              {races.map((race) => (
-                <RaceCard
-                  key={race.id}
-                  race={race}
-                  isSelected={race.id === selectedRace.id}
-                  onSelect={handleSelectRace}
-                />
-              ))}
-            </div>
-            <div className="flex-shrink-0">
-              <RaceHeader
-                raceName={`${selectedRace.country} GP` || "Select a race"}
-                raceDate={new Date(selectedRace.date)}
-                onEdit={toggleEdit}
-                onViewBreakdown={handleOpenBreakdown}
-              />
-            </div>
-            <div className="flex flex-col flex-1 min-h-0">
-              {selectedDrivers.length > 0 ? (
-                <div className="flex-1 flex items-center justify-center">
-                  <CardStack
-                    cards={selectedDrivers.map((driver) => (
-                      <DriverCard
-                        driver={driver}
-                        key={driver.driverCode}
-                        rating={0}
-                        race={0}
-                        consistency={0}
-                        reliability={0}
-                        AI_Qualifying={""}
-                        AI_Race={""}
-                      />
-                    ))}
+        {loading && <div className="text-center py-4">Loading drivers...</div>}
+        {error && <div className="text-center py-4 text-red-500">{error}</div>}
+        {!loading &&
+          !error &&
+          (!isEditing ? (
+            <>
+              <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-2.5 flex-shrink-0">
+                {races.map((race) => (
+                  <RaceCard
+                    key={race.id}
+                    race={race}
+                    isSelected={race.id === selectedRace.id}
+                    onSelect={handleSelectRace}
                   />
+                ))}
+              </div>
+              <div className="flex-shrink-0">
+                <RaceHeader
+                  raceName={`${selectedRace.country} GP` || "Select a race"}
+                  raceDate={new Date(selectedRace.date)}
+                  onEdit={toggleEdit}
+                  onViewBreakdown={handleOpenBreakdown}
+                />
+              </div>
+              <div className="flex flex-col flex-1 min-h-0">
+                {selectedDrivers.length > 0 ? (
+                  <div className="flex-1 flex items-center justify-center">
+                    <CardStack
+                      cards={selectedDrivers.map((driver) => (
+                        <DriverCard
+                          driver={driver}
+                          key={driver.driverCode}
+                          rating={0}
+                          race={0}
+                          consistency={0}
+                          reliability={0}
+                          AI_Qualifying={""}
+                          AI_Race={""}
+                        />
+                      ))}
+                    />
+                  </div>
+                ) : (
+                  <div className="flex-1 flex items-center justify-center ring-1 ring-white/10 rounded-2xl mb-4">
+                    <p className="font-black text-lg font-[Unbounded]">
+                      No Fantasy built!
+                    </p>
+                  </div>
+                )}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex-shrink-0">
+                <RaceHeader
+                  raceName={`${selectedRace.country}` || "Select a race"}
+                  raceDate={new Date(selectedRace.date)}
+                  onEdit={toggleEdit}
+                />
+              </div>
+              {selectedDrivers.length > 0 ? (
+                <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-2 flex-shrink-0">
+                  <AnimatePresence>
+                    {selectedDrivers.map((driver, index) => (
+                      <motion.div
+                        key={driver.driverCode}
+                        initial={{ opacity: 0, x: 20 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -20 }}
+                        transition={{
+                          duration: 0.2,
+                          delay: index * 0.05, // Stagger the animations
+                          ease: "easeInOut",
+                        }}
+                      >
+                        <DriverCardMini
+                          driver={driver}
+                          removeDriver={removeDriver}
+                        />
+                      </motion.div>
+                    ))}
+                  </AnimatePresence>
                 </div>
               ) : (
-                <div className="flex-1 flex items-center justify-center ring-1 ring-white/10 rounded-2xl mb-4">
+                <div className="flex flex-col items-center justify-center ring-1 ring-white/10 rounded-2xl h-[120px] gap-4 p-4 flex-shrink-0">
                   <p className="font-black text-lg font-[Unbounded]">
-                    No Fantasy built!
+                    Draft your lineup below!
                   </p>
+                  <TbArrowBigDownLine className="animate-bounce" size={24} />
                 </div>
               )}
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="flex-shrink-0">
-              <RaceHeader
-                raceName={`${selectedRace.country}` || "Select a race"}
-                raceDate={new Date(selectedRace.date)}
-                onEdit={toggleEdit}
-              />
-            </div>
-            {selectedDrivers.length > 0 ? (
-              <div className="flex flex-row overflow-x-auto no-scrollbar justify-start gap-2 flex-shrink-0">
-                <AnimatePresence>
-                  {selectedDrivers.map((driver, index) => (
-                    <motion.div
-                      key={driver.driverCode}
-                      initial={{ opacity: 0, x: 20 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      exit={{ opacity: 0, x: -20 }}
-                      transition={{
-                        duration: 0.2,
-                        delay: index * 0.05, // Stagger the animations
-                        ease: "easeInOut",
-                      }}
-                    >
-                      <DriverCardMini
-                        driver={driver}
-                        removeDriver={removeDriver}
-                      />
-                    </motion.div>
-                  ))}
-                </AnimatePresence>
+              <div className="flex-1 min-h-0">
+                <DriverSelector />
               </div>
-            ) : (
-              <div className="flex flex-col items-center justify-center ring-1 ring-white/10 rounded-2xl h-[120px] gap-4 p-4 flex-shrink-0">
-                <p className="font-black text-lg font-[Unbounded]">
-                  Draft your lineup below!
-                </p>
-                <TbArrowBigDownLine className="animate-bounce" size={24} />
-              </div>
-            )}
-            <div className="flex-1 min-h-0">
-              <DriverSelector />
-            </div>
-          </>
-        )}
+            </>
+          ))}
       </div>
       <BreakdownModal
         isOpen={isViewingBreakdown}
