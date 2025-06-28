3207398e541b49357700cf7e12f5ecc2700825e0
Samar Khan <skhan511@umd.edu>
2025-06-28 04:22:35 -0400

    removed dependency on hardcoded driverlist

--- src/components/driver-selector/DriverSelector.tsx
+++ src/components/driver-selector/DriverSelector.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import { driverlist } from "../../data/driverlist";
 import BottomSheet from "../bottom-sheet/BottomSheet";
 import DriverCardSelector from "../driver-card-selector/DriverCardSelector";
 
 const DriverSelector = () => {
+  const [drivers, setDrivers] = useState<Driver[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchDrivers = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("http://localhost:8000/api/drivers");
+        const drivers = await response.json();
+        setDrivers(drivers);
+      } catch (err) {
+        setError("Failed to fetch races");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDrivers();
+  }, []);
+
   return (
     <BottomSheet baseHeightRatio={0.55} expandedHeightRatio={0.8}>
       <div className="flex flex-col gap-2">
-        {driverlist.map((driver) => (
+        {drivers.map((driver) => (
           <DriverCardSelector driver={driver} key={driver.driverCode} />
         ))}
       </div>
