c8519a3088e021172dc15742d43a2cc4f83bb6d2
Samar Khan <skhan511@umd.edu>
2025-07-09 00:25:07 -0400

    ui

--- src/components/race-header/RaceHeader.tsx
+++ src/components/race-header/RaceHeader.tsx
@@ -19,22 +19,23 @@ interface TimeStatus {
 }
 
 const getTimeStatus = (raceDate: Date): TimeStatus => {
-  const now = new Date();
+  const now = Date.now();
+  const diff = raceDate.getTime() - now;
 
-  const diff = raceDate.getTime() - now.getTime();
+  const totalHours = Math.floor(diff / (1000 * 60 * 60));
 
   // Convert to days, hours, minutes
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
 
-  if (hours < -2) {
+  if (totalHours < -2) {
     return {
       text: "Finished",
       icon: <TbFlag />,
       editable: false,
     };
-  } else if (hours < 0) {
+  } else if (totalHours < 0) {
     return {
       text: "In Progress",
       editable: false,
